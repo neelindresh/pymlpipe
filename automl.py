9cf607daa89c15a00a0d943a48d31dd2c9bc264f
Dnik007 <debanik07@gmail.com>
2023-01-30 15:21:55 +0530

    automl framework changes

--- automl.py
+++ automl.py
@@ -1,5 +1,6 @@
-from sklearn.datasets import  load_iris,load_diabetes
+from pymlpipe.tabular import PyMLPipe
 import pandas as pd
+from tqdm import tqdm
 from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier,BaggingRegressor,AdaBoostRegressor, ExtraTreesRegressor, RandomForestRegressor,GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression,PassiveAggressiveClassifier, RidgeClassifier, SGDClassifier,LinearRegression, Lasso, Ridge, ElasticNet, BayesianRidge, HuberRegressor, PoissonRegressor,PassiveAggressiveRegressor
@@ -10,13 +11,13 @@ from xgboost import XGBClassifier, XGBRegressor
 from catboost import CatBoostClassifier, CatBoostRegressor
 from lightgbm import LGBMClassifier, LGBMRegressor
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,r2_score,mean_absolute_error,mean_squared_error,make_scorer
-#import PyMLPipe from tabular 
-from pymlpipe.tabular import PyMLPipe
+#from tabular import PyMLPipe
 from sklearn.model_selection import GridSearchCV
+from itertools import chain
 
 
 class AutoMLPipe():
-    def __init__(self,exp_name,task,metric,data,label,tags=[],tune=True,tune_best=False,test_size=0.20,version=1.0,register_model=False,register_artifacts=False,exclude=[]):
+    def __init__(self,exp_name,task,metric,data,label,tags=[],test_size=0.20,version=1.0,register_model=False,register_artifacts=False,exclude=[]):
         '''
         exp_name: name of experiment
         task: regression/classification
@@ -24,8 +25,6 @@ class AutoMLPipe():
         data: data on which the model to be fit
         label: target variable
         tags: list of custom-tags for the run
-        tune: param tune all the models
-        tune_best: param tune the best model
         test_size: size of test dataset
         version: experiment version
         register_model: register experiement model
@@ -44,8 +43,6 @@ class AutoMLPipe():
         self.register_model=register_model
         self.register=register_artifacts
         self.tags=tags
-        self.tune=tune
-        self.tune_best=tune_best
         self.classification_models={
                 'log_reg': LogisticRegression(),
                 'adac':AdaBoostClassifier(),
@@ -227,11 +224,15 @@ class AutoMLPipe():
         }
         self.param_grid['cbr']={
             'n_estimators': [10,100,500],
-            'max_depth': [None,2,3],
+            'max_depth': [2,3],
            'learning_rate': [0.01,0.1]
         }   
 
-    def run_automl(self):
+    def run_automl(self,tune=False,tune_best=False):
+        '''
+        tune: param tune all the models
+        tune_best: param tune the best model
+        '''
 
         # Set Experiment name
         self.mlp.set_experiment(self.exp_name)
@@ -241,42 +242,47 @@ class AutoMLPipe():
         
         
         result=pd.DataFrame()
+        prediction_set={}
         if self.task=='classification':
-            for model_name,model in self.classification_models.items():
-                if model_name not in self.exclude:
+            for model_name,model in tqdm(self.classification_models.items()):
+                if model_name not in self.exclude:        
+                    if tune==True:
+                            
+                        predictions,result_set=self.param_tune_model(model_name,trainx,testx,trainy,testy)
+                        predictions=predictions.tolist()
+                        if model_name=='cbc':
+                            predictions=list(chain(*predictions))
+                        fin=dict()
+                        fin['name']=model_name
+                        fin['accuracy']=result_set['accuracy']
+                        fin['precision']=result_set['precision']
+                        fin['recall']=result_set['recall']
+                        fin['f1']=result_set['f1_score']
+                    
+                    else:
                         with self.mlp.run():
-                            # set tags
                             default_tags=[model_name,"Classification"]
                             tag_list=default_tags+self.tags
                             self.mlp.set_tags(tag_list)
-                            if self.tune==True:
-                                
-                                    final_model=model.fit(trainx, trainy)
-                                    if self.metric=='accuracy': score= make_scorer(accuracy_score,average='weighted')
-                                    elif self.metric=='recall': score=make_scorer(recall_score,average='weighted')
-                                    elif self.metric=='precision': score=make_scorer(precision_score,average='weighted')
-                                    else: score=make_scorer(f1_score,average='weighted')
-                                    model = GridSearchCV(estimator = final_model, param_grid = self.param_grid[model_name], scoring=score,verbose = 2)
-                                    model.fit(trainx, trainy)
-                                    predictions=model.best_estimator_.predict(testx)
-                            
-                            else:
-                           
-                                model=model
-                                model.fit(trainx, trainy)
-                                predictions=model.predict(testx)
+                        
+                            model=model
+                            model.fit(trainx, trainy)
+                            predictions=model.predict(testx)
+                            predictions=predictions.tolist()
+                            if model_name=='cbc':
+                                predictions=list(chain(*predictions))
 
-                            self.mlp.log_metric("Accuracy", accuracy_score(testy,predictions))
-                            self.mlp.log_metric("Precision", precision_score(testy,predictions,average='macro'))
-                            self.mlp.log_metric("Recall", recall_score(testy,predictions,average='macro'))
-                            self.mlp.log_metric("F1", f1_score(testy,predictions,average='macro'))
+                            self.mlp.log_metric("accuracy", accuracy_score(testy,predictions))
+                            self.mlp.log_metric("precision", precision_score(testy,predictions,average='macro'))
+                            self.mlp.log_metric("recall", recall_score(testy,predictions,average='macro'))
+                            self.mlp.log_metric("f1_score", f1_score(testy,predictions,average='macro'))
+
                             if self.register==True:
-                                # Save train data and test data
                                 self.mlp.register_artifact("train", trainx)
                                 self.mlp.register_artifact("test", testx,artifact_type="testing")
                             if self.register_model==True:
-                                 self.mlp.scikit_learn.register_model(model_name, model)
-                            
+                                self.mlp.scikit_learn.register_model(model_name, model)
+                        
                             result1=self.mlp.get_info()
                             fin=dict()
                             fin['name']=model_name
@@ -284,28 +290,31 @@ class AutoMLPipe():
                             fin['precision']=result1['metrics']['Precision']
                             fin['recall']=result1['metrics']['Recall']
                             fin['f1']=result1['metrics']['F1']
-                            result=result.append(fin,ignore_index=True)
+                    
+                    prediction_set[model_name]=predictions
+                    result=result.append(fin,ignore_index=True)
         elif self.task=='regression':
-            for model_name,model in self.regression_models.items():
+            for model_name,model in tqdm(self.regression_models.items()):
                 if model_name not in self.exclude:
+                    if tune==True:
+                        predictions,result_set=self.param_tune_model(model_name,trainx,testx,trainy,testy)
+                        predictions=predictions.tolist()
+                        fin=dict()
+                        fin['name']=model_name
+                        fin['MAE']=result_set['MAE']
+                        fin['MSE']=result_set['MSE']
+                        fin['R2 Score']=result_set['R2 Score']
+                        fin['RMSE']=result_set['RMSE']
+                    else:
                         with self.mlp.run():
-                            # set tags
                             default_tags=[model_name,"Regression"]
                             tag_list=default_tags+self.tags
                             self.mlp.set_tags(tag_list)
-                            if self.tune==True:
-                                final_model=model.fit(trainx, trainy)
-                                if self.metric=='MSE': score= 'neg_mean_squared_error'
-                                elif self.metric=='MAE': score='neg_mean_absolute_error'
-                                elif self.metric=='R2 Score': score='r2'
-                                else: score='neg_root_mean_squared_error'
-                                model = GridSearchCV(estimator = final_model, param_grid = self.param_grid[model_name], scoring=score,verbose = 2)
-                                model.fit(trainx, trainy)
-                                predictions=model.best_estimator_.predict(testx)
-                            else:
-                                model=model
-                                model.fit(trainx, trainy)
-                                predictions=model.predict(testx)
+                            model=model
+                            model.fit(trainx, trainy)
+                            predictions=model.predict(testx)
+                            predictions=predictions.tolist()
+                         
                             # log performace metrics
                             self.mlp.log_metric("R2 Score", r2_score(testy,predictions))
                             self.mlp.log_metric("MAE", mean_absolute_error(testy,predictions))
@@ -317,41 +326,39 @@ class AutoMLPipe():
                                 self.mlp.register_artifact("test", testx,artifact_type="testing")
                             # Save the model
                             if self.register_model==True:
-                                 self.mlp.scikit_learn.register_model(model_name, model)
+                                    self.mlp.scikit_learn.register_model(model_name, model)
                             result1=self.mlp.get_info()
-                            
+                        
                             fin=dict()
                             fin['name']=model_name
                             fin['MAE']=result1['metrics']['MAE']
                             fin['MSE']=result1['metrics']['MSE']
                             fin['RMSE']=result1['metrics']['RMSE']
                             fin['R2 Score']=result1['metrics']['R2 Score']
-                            #fin['runid']=self.mlp.runid()
-                            #result=result.append(result1,ignore_index=True)
-                            result=result.append(fin, ignore_index=True)    
+                    
+                    prediction_set[model_name]=predictions  
+                    result=result.append(fin, ignore_index=True)    
                
         if self.task=='classification' or self.metric=='R2 Score': 
             result.sort_values(by=self.metric,ascending=False,inplace=True)
         else:
             result.sort_values(by=self.metric,ascending=True,inplace=True)
             
-        if self.tune_best==False:
-            return result
+        if tune_best==False:
+            return prediction_set,result
         else:
             result=result.head(1)
             best_model_name=str(result.name.values[0])
             
-            self.param_tune_model(model_tune=best_model_name)
-       
+            prediction_set,result=self.param_tune_model(trainx=trainx,testx=testx,trainy=trainy,testy=testy,model_tune=best_model_name)
+            return prediction_set,result
 
-    def param_tune_model(self,model_tune):
+    def param_tune_model(self,model_tune,trainx,testx,trainy,testy):
        
         self.mlp.set_experiment(self.exp_name)    
         best_model_name=model_tune
         
         with self.mlp.run():
-            trainx,testx,trainy,testy=trainx,testx,trainy,testy=train_test_split(self.data,self.label,test_size=self.test_size)
-            print(best_model_name)
             if self.task=="classification":
                 default_tags=["Hyper-param-tuning-clf",best_model_name]
                 tag_list=default_tags+self.tags
@@ -361,9 +368,17 @@ class AutoMLPipe():
                 elif self.metric=='recall': score=make_scorer(recall_score,average='weighted')
                 elif self.metric=='precision': score=make_scorer(precision_score,average='weighted')
                 else: score=make_scorer(f1_score,average='weighted')
-                CV_cfl = GridSearchCV(estimator = final_model, param_grid = self.param_grid[best_model_name], scoring= score, verbose = 2)
+                CV_cfl = GridSearchCV(estimator = final_model, param_grid = self.param_grid[best_model_name], scoring= score, cv=3, verbose = 2)
                 CV_cfl.fit(trainx, trainy)
-                self.mlp.scikit_learn.register_model(best_model_name, CV_cfl)
+                self.mlp.log_params(CV_cfl.best_params_)
+                predictions=CV_cfl.best_estimator_.predict(testx)
+
+                result_set={
+                    "accuracy": accuracy_score(testy,predictions),
+                    "precision": precision_score(testy,predictions,average='macro'),
+                    "recall": recall_score(testy,predictions,average='macro'),
+                    "f1_score": f1_score(testy,predictions,average='macro')}
+                self.mlp.log_metrics(result_set)
             elif self.task=="regression":
                 default_tags=["Hyper-param-tuning-reg",best_model_name]
                 tag_list=default_tags+self.tags
@@ -373,27 +388,24 @@ class AutoMLPipe():
                 elif self.metric=='MAE': score='neg_mean_absolute_error'
                 elif self.metric=='R2 Score': score='r2'
                 else: score='neg_root_mean_squared_error'
-                CV_cfl = GridSearchCV(estimator = final_model, param_grid = self.param_grid[best_model_name], scoring=score,verbose = 2)
+                CV_cfl = GridSearchCV(estimator = final_model, param_grid = self.param_grid[best_model_name], scoring=score,cv=3,verbose = 2)
                 CV_cfl.fit(trainx, trainy)
+                self.mlp.log_params(CV_cfl.best_params_)
+                predictions=CV_cfl.best_estimator_.predict(testx)
+
+                result_set={
+                    "MSE": mean_squared_error(testy,predictions),
+                    "MAE": mean_absolute_error(testy,predictions),
+                    "R2 Score": r2_score(testy,predictions),
+                    "RMSE": mean_squared_error(testy,predictions,squared=False)}
+                self.mlp.log_metrics(result_set)
+            if self.register_model==True:
                 self.mlp.scikit_learn.register_model(best_model_name, CV_cfl)
+            if self.register==True:
+                # Save train data and test data
+                self.mlp.register_artifact("train", trainx)
+                self.mlp.register_artifact("test", testx,artifact_type="testing")
 
-            best_parameters = CV_cfl.best_params_
-            print(CV_cfl.best_score_)
-            #cfl=
-            print("The best parameters for using this model is", best_parameters)
-            predictions=CV_cfl.best_estimator_.predict(testx)
-            print(predictions)
-            self.mlp.log_params(CV_cfl.best_params_)
-            # log performace metrics
-            if self.task=="classification":
-                self.mlp.log_metric("Accuracy", accuracy_score(testy,predictions))
-                self.mlp.log_metric("Precision", precision_score(testy,predictions,average='macro'))
-                self.mlp.log_metric("Recall", recall_score(testy,predictions,average='macro'))
-                self.mlp.log_metric("F1", f1_score(testy,predictions,average='macro'))
-            elif self.task=="regression":
-                self.mlp.log_metric("MAE", mean_absolute_error(testy,predictions))
-                self.mlp.log_metric("MSE", mean_squared_error(testy,predictions))
-                self.mlp.log_metric("RMSE", mean_squared_error(testy,predictions,squared=False))
-                self.mlp.log_metric("R2 Score", r2_score(testy,predictions))
-
-            return predictions
+            return predictions,result_set
+        
+       
